d4f824da3ac757d084f9033b8e691e2a05f1bea6
Seadra <colin.rexing@gmail.com>
2018-03-08 14:42:02 -0800

    Moved class

--- CST126SRS04/WICLL/main.cpp
+++ CST126SRS04/WICLL/main.cpp
@@ -1,22 +1,20 @@
 ﻿#include "stdafx.h"
 #include <string>
-#include <iostream>
+
+class CSWoman
+{
+private:
+	const std::string name_;
+	const unsigned birthYear_;
+	const std::string fact_;
+
+public:
+	CSWoman(const char* const name, const unsigned birthYear, const char* const fact);
+	// ... and other methods  
+};
 
 int main()
 {
-	class CSWoman
-	{
-	private:
-		const std::string name_;
-		const unsigned birthYear_;
-		const std::string fact_;
 
-	public:
-		CSWoman(const char* const name, const unsigned birthYear, const char* const fact);
-		// ... and other methods  
-	};
-
-	auto* const ada = new CSWoman("Ada Lovelace", 1815, "Prophet of computer age");
-	
 	return 0;
 }
