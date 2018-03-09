8e63c789a5f453d7bf5a80f837b0f32e1dda09ca
Seadra <colin.rexing@gmail.com>
2018-03-08 21:07:38 -0800

    updated const

--- WICLL/main.cpp
+++ WICLL/main.cpp
@@ -1,11 +1,12 @@
 ﻿#include "stdafx.h"
 #include "CSWoman.h"
 #include "LinkList.h"
+#include <iostream>
 
 
 int main()
 {
-	auto w1 = new CSWoman("ada lovelace", 1815, "unknown");
+	const auto w1 = new CSWoman("ada lovelace", 1815, "unknown");
     return 0;
 }
 
