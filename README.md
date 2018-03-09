9ba3b90f2c0e500954e8be33361b31c7b141e9a3
Beau Bell <beauski00@live.com>
2018-03-08 19:41:17 -0800

    setting things up

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Women In Computing Linked List  
 
 
-Class ID: 
+Class ID: Phantump
 
 SRS ID: CST126SRS04  
 
---
+++ WILCC/CSWoman.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+
+class CSWoman
+{
+private:
+	const std::string name_;
+	const unsigned birthYear_;
+	const std::string fact_;
+
+public:
+	CSWoman(const char* const name, const unsigned birthYear, const char* const fact);
+};
