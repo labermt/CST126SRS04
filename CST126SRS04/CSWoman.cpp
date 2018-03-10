c6653c79edecef668e236fceb02381734cfeef6a
an6688 <anil.coklar@oit.edu>
2018-03-10 13:43:26 -0800

    update...again

--- CST126SRS04/CSWoman.cpp
+++ CST126SRS04/CSWoman.cpp
@@ -7,6 +7,21 @@ CSWoman::CSWoman(const char * const name, const unsigned birthYear, const char *
 {
 }
 
+std::string CSWoman::getName() const
+{
+	return name_;
+}
+
+unsigned CSWoman::getBirthYear() const
+{
+	return birthYear_;
+}
+
+std::string CSWoman::getFact() const
+{
+	return fact_; 
+}
+
 std::ostream & operator<<(std::ostream & os, CSWoman const & lady )
 {
 	os << lady.getName() << ", " << lady.getBirthYear() << ", " << lady.getFact() << ", " << std::endl;
