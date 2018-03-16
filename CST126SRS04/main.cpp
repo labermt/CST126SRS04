5bacd1165aacb7e5a78c49086f74cfc6168b86a4
an6688 <anil.coklar@oit.edu>
2018-03-15 21:42:46 -0700

    final update
    i thnk so

--- CST126SRS04/main.cpp
+++ CST126SRS04/main.cpp
@@ -12,16 +12,16 @@
 //I would be lost without Mitch for this assignment. Thank you 
 int main()
 {
-	CSWoman * woman0{ new CSWoman("Radia Perlman", 1951, "Grew up in NJ\n") };
-	CSWoman * woman1{ new CSWoman("Carol Shaw", 1955, "In 2017, she won the Industry Icon Award at The Game Awards\n") };
-	CSWoman * woman2{ new CSWoman("Susan Kare", 1954, "She is the designer of many typefaces, icons, and original marketing material for the original Macintosh OS\n") };
-	CSWoman * woman3{ new CSWoman("Grace Hopper", 1906, "Google made the Google Doodle for her 107th birthday\n") };
-	CSWoman * woman4{ new CSWoman("Janese Swanson", 1958, "Wrote a thesis on gender issues in product design, play patterns and gender preferences\n") };
-	CSWoman * woman5{ new CSWoman("Beatrice Worsley", 1921, "In 2014, she was posthumously awarded the Lifetime Achievement Award in Computer Science\n") };
-	CSWoman * woman6{ new CSWoman("Dorothy Vaughan", 1910, "She worked for opportunities for the women in West Computing\n") };
-	CSWoman * woman7{ new CSWoman("Sophie Wilson", 1957, "designed a microcomputer with a 6502 microprocessor\n") };
-	CSWoman * woman8{ new CSWoman("Anita Borg", 1949, "She founded the Institute for Women and Technology and the Grace Hopper Celebration of Women in Computing\n") };
-	CSWoman * woman9{ new CSWoman("Lucy Sanders", 1954, "co-founded the National Center for Women & Information Technology\n") };
+	auto const woman0{ new const CSWoman("Radia Perlman", 1951, "Grew up in NJ\n") };
+	auto const woman1{ new const CSWoman("Carol Shaw", 1955, "In 2017, she won the Industry Icon Award at The Game Awards\n") };
+	auto const woman2{ new const CSWoman("Susan Kare", 1954, "She is the designer of many typefaces, icons, and original marketing material for the original Macintosh OS\n") };
+	auto const woman3{ new const CSWoman("Grace Hopper", 1906, "Google made the Google Doodle for her 107th birthday\n") };
+	auto const woman4{ new const CSWoman("Janese Swanson", 1958, "Wrote a thesis on gender issues in product design, play patterns and gender preferences\n") };
+	auto const woman5{ new const CSWoman("Beatrice Worsley", 1921, "In 2014, she was posthumously awarded the Lifetime Achievement Award in Computer Science\n") };
+	auto const woman6{ new const CSWoman("Dorothy Vaughan", 1910, "She worked for opportunities for the women in West Computing\n") };
+	auto const woman7{ new const CSWoman("Sophie Wilson", 1957, "designed a microcomputer with a 6502 microprocessor\n") };
+	auto const woman8{ new const CSWoman("Anita Borg", 1949, "She founded the Institute for Women and Technology and the Grace Hopper Celebration of Women in Computing\n") };
+	auto const woman9{ new const CSWoman("Lucy Sanders", 1954, "co-founded the National Center for Women & Information Technology\n") };
 	 
 	if 
 	(
@@ -54,7 +54,6 @@ int main()
 		ll.reverse();
 		std::cout << ll << std::endl;
 	} 
-
 	_CrtDumpMemoryLeaks();
 	system("pause");
     return 0;
