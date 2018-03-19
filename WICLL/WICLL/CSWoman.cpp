e818304d5942a4f012cbc51597b8c21ecc034112
Mitch Laber <labermt@gmail.com>
2018-03-19 01:34:01 -0700

    nullptr check.

--- WICLL/WICLL/CSWoman.cpp
+++ WICLL/WICLL/CSWoman.cpp
@@ -1,12 +1,14 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <cassert>
 #include "CSWoman.h"
 
 CSWoman::CSWoman(const char* const name, const unsigned birthYear, const char* const fact)
-	: name_{ name }, birthYear_{ birthYear }, fact_{ fact }
+	: name_{ name?name:"" }, birthYear_{ birthYear?birthYear:"" }, fact_{ fact?fact:"" }
 {
-
+	assert(name);
+	assert(fact);
 }
 
 // CSWoman::~CSWoman() {}
